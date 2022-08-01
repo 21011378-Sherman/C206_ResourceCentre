915e459600b09ca953790a404d84c6508e04e169
21011378 <21011378@10.174.115.93>
2022-08-01 10:27:39 +0800

    updated

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -237,7 +237,7 @@ public class ResourceCentreTest {
 		
 		//error
 		isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0013");
-		assertFalse("Test if non-existing amcorder CB0013 is returned - false?", isReturned);
+		assertFalse("Test if non-existing Chromebook CB0013 is returned - false?", isReturned);
 				
 		
 	}
